ccf629e07f64cae59340171c25fa7e4bd56a783b
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-08-13 23:20:52 -0500

    refactor to use a controller

--- dev-skills-lab/routes/skills.js
+++ dev-skills-lab/routes/skills.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var Skill = require("../models/skill");
+var skillsCtrl = require("../controllers/skills");
 
-router.get("/", function(req, res) {
-  res.render("skills/index", {
-    skills: Skill.getAll()
-  });
-});
+router.get("/", skillsCtrl.index);
 
 module.exports = router;
